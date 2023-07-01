5966e44389be3f50d4fbc110a3ba0c96709da8f5
rishaviitd <103333413+rishaviitd@users.noreply.github.com>
2023-07-01 05:32:36 +0530

    sign up error and submission uploded

--- integration/daksha/static/daksha/view.js
+++ integration/daksha/static/daksha/view.js
@@ -47,3 +47,33 @@
             mobileNotificationDown.classList.remove('active');
           }
         });
+
+      var submitButton = document.getElementById("submitButton");
+      var closeButton = document.querySelector(".alert button");
+      var alertContainer = document.querySelector(".alert-container");
+      var alert = document.querySelector(".alert");
+      
+      submitButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent form submission
+        
+        alertContainer.style.display = "flex";
+        alert.style.display = "block";
+      });
+
+      closeButton.addEventListener("click", function() {
+        alertContainer.style.display = "none";
+        alert.style.display = "none";
+      });
+
+      var fileInput = document.getElementById("zip-file");
+      var uploadText = document.querySelector(".upload-text");
+      
+      fileInput.addEventListener("change", function() {
+        var files = fileInput.files;
+        if (files.length > 0) {
+          var name = files[0].name;
+          uploadText.textContent = "File uploaded: " + name;
+        } else {
+          uploadText.textContent = "Click to upload ZIP file";
+        }
+      });
